93ce5dff4cd5c2056c6aa1e24652b072c54b0fde
Sundarjai2 <sundar.jayadoss@gmail.com>
2022-01-18 12:01:22 +0530

    html added

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormField } from '../form-field';
 import { FormfieldControlService } from '../formfield-control.service';
